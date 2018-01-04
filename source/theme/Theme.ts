a500133dc02c1d35b8eb4dd10597270464a5b64d
Barış Ozan Güler <baris@teamsql.io>
2018-01-04 17:30:50 +0300

    theme support new properties

--- source/theme/Theme.ts
+++ source/theme/Theme.ts
@@ -1,3 +1,8 @@
 export class Theme {
     public json: string;
+    public id: string;
+    public name: string;
+    public version: string;
+    public description: string;
+    public author: string;
 }
